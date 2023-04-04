0697a7846148f7f19e237521c72b615e781e159c
selvidugan <selvidugan@gmail.com>
2023-04-04 22:19:25 +0300

    portfolio to push to github

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
 import React, {useState} from 'react'
 import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
-// import { HiOutlineMail } from 'react-icons/hi';
-// import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/sd-logot.png'
 import {Link} from 'react-scroll'
 
@@ -74,18 +72,6 @@ const Navbar = () => {
                         GitHub <FaGithub className='mr-[-10px] lg:mr-0 duration-300' size={30} />
                     </a>
                 </li>
-                {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-                    <a className='flex justify-between items-center w-full text-gray-300'
-                    href="/">
-                        E-mail <HiOutlineMail size={30} />
-                    </a>
-                </li> */}
-                {/* <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                    <a className='flex justify-between items-center w-full text-gray-300'
-                    href="/">
-                        Resume <BsFillPersonLinesFill size={30} />
-                    </a>
-                </li> */}
             </ul>
 
         </div>
